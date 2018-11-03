bb22f5fb6405e8f8c83d1a6c44c864d50bde5912
p32929 <p32929@yahoo.com>
2018-11-03 14:45:57 +0600

    added tappx

--- public/js/main.js
+++ public/js/main.js
@@ -13,121 +13,122 @@
     limitations under the License. */
 
 var vueapp = new Vue({
-  el: '#vueapp',
-  data: {
-    iOrWe: '[I/We]',
-    typeOfApp: '',
-    typeOfAppTxt: '[open source/free/freemium/ad-supported/commercial]',
-    typeOfDev: '',
-    appName: '[App Name]',
-    myOrOur: '[my/our]',
-    meOrUs: '[me/us]',
-    atNoCost: '[at no cost]',
-    retainedInfo: '[retained on your device and is not collected by [me/us] in any way]/[retained by us and used as described in this privacy policy]',
-    devName: '',
-    companyName: '',
-    devOrCompanyName: '[Developer/Company name]',
-    pidInfoIn: '',
-    pidInfo: '[add whatever else you collect here, e.g. users name, address, location, pictures]',
-    gps: true,
-    admob: false,
-    firebase_analytics: false,
-    piwik: false,
-    fabric: false,
-    clicky: false,
-    crashlytics: false,
-    facebook: false,
-    osType: '',
-    requirementOfSystem: 'system',
-    thirdPartyServices: thirdPartyServicesJsonArray
-  },
-  methods: {
-    selectAllText: function (id) {
-      selectText(id)
+    el: '#vueapp',
+    data: {
+        iOrWe: '[I/We]',
+        typeOfApp: '',
+        typeOfAppTxt: '[open source/free/freemium/ad-supported/commercial]',
+        typeOfDev: '',
+        appName: '[App Name]',
+        myOrOur: '[my/our]',
+        meOrUs: '[me/us]',
+        atNoCost: '[at no cost]',
+        retainedInfo: '[retained on your device and is not collected by [me/us] in any way]/[retained by us and used as described in this privacy policy]',
+        devName: '',
+        companyName: '',
+        devOrCompanyName: '[Developer/Company name]',
+        pidInfoIn: '',
+        pidInfo: '[add whatever else you collect here, e.g. users name, address, location, pictures]',
+        gps: true,
+        admob: false,
+        tappx: false,
+        firebase_analytics: false,
+        piwik: false,
+        fabric: false,
+        clicky: false,
+        crashlytics: false,
+        facebook: false,
+        osType: '',
+        requirementOfSystem: 'system',
+        thirdPartyServices: thirdPartyServicesJsonArray
     },
-    toggleState: function (item) {
-      let state = item.model
+    methods: {
+        selectAllText: function (id) {
+            selectText(id)
+        },
+        toggleState: function (item) {
+            let state = item.model
 
-      console.log('Item:', item.name, item.model, item[state])
-      // For reactive update of the json
-      // Toggle the state
-      Vue.set(thirdPartyServicesJsonArray, item.model, !item[state])
-    },
-    generateHTML: function (id, filename) {
-      let content = getContent(id)
-      let title = getTitle(id)
-      let rawHTML = getRawHTML(content, title)
-      downloadHTML(filename, rawHTML)
-    },
-    generateMD: function (id, filename) {
-      let content = getContent(id)
-      let title = getTitle(id)
-      let rawHTML = getRawHTML(content, title)
-      let markdown = convertHtmlToMd(rawHTML)
-      downloadMD(filename, markdown)
-    },
-    generate: function () {
-      if (this.typeOfDev === 'Individual') {
-        this.devOrCompanyName = this.devName
-        this.iOrWe = 'I'
-        this.myOrOur = 'my'
-        this.meOrUs = 'me'
-        this.retainedInfo =
-          'retained on your device and is not collected by ' +
-          this.meOrUs +
-          ' in any way'
-      } else if (this.typeOfDev === 'Company') {
-        this.devOrCompanyName = this.companyName
-        this.iOrWe = 'we'
-        this.myOrOur = 'our'
-        this.meOrUs = 'us'
-        this.retainedInfo =
-          'retained by us and used as described in this privacy policy'
-      }
+            console.log('Item:', item.name, item.model, item[state])
+            // For reactive update of the json
+            // Toggle the state
+            Vue.set(thirdPartyServicesJsonArray, item.model, !item[state])
+        },
+        generateHTML: function (id, filename) {
+            let content = getContent(id)
+            let title = getTitle(id)
+            let rawHTML = getRawHTML(content, title)
+            downloadHTML(filename, rawHTML)
+        },
+        generateMD: function (id, filename) {
+            let content = getContent(id)
+            let title = getTitle(id)
+            let rawHTML = getRawHTML(content, title)
+            let markdown = convertHtmlToMd(rawHTML)
+            downloadMD(filename, markdown)
+        },
+        generate: function () {
+            if (this.typeOfDev === 'Individual') {
+                this.devOrCompanyName = this.devName
+                this.iOrWe = 'I'
+                this.myOrOur = 'my'
+                this.meOrUs = 'me'
+                this.retainedInfo =
+                    'retained on your device and is not collected by ' +
+                    this.meOrUs +
+                    ' in any way'
+            } else if (this.typeOfDev === 'Company') {
+                this.devOrCompanyName = this.companyName
+                this.iOrWe = 'we'
+                this.myOrOur = 'our'
+                this.meOrUs = 'us'
+                this.retainedInfo =
+                    'retained by us and used as described in this privacy policy'
+            }
 
-      if (this.typeOfApp === 'Commercial') {
-        this.atNoCost = ''
-      } else {
-        this.atNoCost = 'at no cost'
-      }
+            if (this.typeOfApp === 'Commercial') {
+                this.atNoCost = ''
+            } else {
+                this.atNoCost = 'at no cost'
+            }
 
-      if (this.pidInfoIn === '') {
-        this.pidInfo = '.'
-      } else {
-        this.pidInfo = ', including but not limited to ' + this.pidInfoIn + '.'
-      }
+            if (this.pidInfoIn === '') {
+                this.pidInfo = '.'
+            } else {
+                this.pidInfo = ', including but not limited to ' + this.pidInfoIn + '.'
+            }
 
-      switch (this.typeOfApp) {
-        case 'Free':
-        case 'Freemium':
-        case 'Commercial':
-          this.typeOfAppTxt = 'a ' + this.typeOfApp
-          break
-        case 'Open Source':
-        case 'Ad Supported':
-          this.typeOfAppTxt = 'an ' + this.typeOfApp
-          break
-      }
+            switch (this.typeOfApp) {
+                case 'Free':
+                case 'Freemium':
+                case 'Commercial':
+                    this.typeOfAppTxt = 'a ' + this.typeOfApp
+                    break
+                case 'Open Source':
+                case 'Ad Supported':
+                    this.typeOfAppTxt = 'an ' + this.typeOfApp
+                    break
+            }
 
-      switch (this.osType) {
-        case 'Android': {
-          this.osType = 'Android'
-          this.requirementOfSystem = 'system'
-          break
+            switch (this.osType) {
+                case 'Android': {
+                    this.osType = 'Android'
+                    this.requirementOfSystem = 'system'
+                    break
+                }
+                case 'iOS': {
+                    this.osType = 'iOS'
+                    this.requirementOfSystem = 'system'
+                    break
+                }
+                case 'Android & iOS': {
+                    this.osType = 'Android & iOS'
+                    this.requirementOfSystem = 'both systems'
+                    break
+                }
+            }
+
+            document.getElementById('privacy_tab').click()
         }
-        case 'iOS': {
-          this.osType = 'iOS'
-          this.requirementOfSystem = 'system'
-          break
-        }
-        case 'Android & iOS': {
-          this.osType = 'Android & iOS'
-          this.requirementOfSystem = 'both systems'
-          break
-        }
-      }
-
-      document.getElementById('privacy_tab').click()
     }
-  }
 })
--- public/js/thirdpartyservices.js
+++ public/js/thirdpartyservices.js
@@ -13,84 +13,94 @@
     limitations under the License. */
 
 var thirdPartyServicesJsonArray = [
-  {
-    name: 'Google Play Services',
-    model: 'gps', // this is stores the name of the model
-    gps: true, // this stores the state of the model, notice the key is the same as the model above. It is required by code.
-    logo: 'images/gps.png',
-    link: {
-      privacy: 'https://www.google.com/policies/privacy/',
-      terms: ''
+    {
+        name: 'Google Play Services',
+        model: 'gps', // this is stores the name of the model
+        gps: true, // this stores the state of the model, notice the key is the same as the model above. It is required by code.
+        logo: 'images/gps.png',
+        link: {
+            privacy: 'https://www.google.com/policies/privacy/',
+            terms: ''
+        }
+    },
+    {
+        name: 'AdMob',
+        model: 'admob',
+        admob: false,
+        logo: 'images/admob.png',
+        link: {
+            privacy: 'https://support.google.com/admob/answer/6128543?hl=en',
+            terms: ''
+        }
+    },
+    {
+        name: 'Tappx',
+        model: 'tappx',
+        tappx: false,
+        logo: 'images/tappx.png',
+        link: {
+            privacy: 'https://www.tappx.com/en/privacy-policy/',
+            terms: ''
+        }
+    },
+    {
+        name: 'Firebase Analytics',
+        model: 'firebase',
+        firebase: false,
+        logo: 'images/firebase.png',
+        link: {
+            privacy: 'https://firebase.google.com/policies/analytics',
+            terms: ''
+        }
+    },
+    {
+        name: 'Facebook',
+        model: 'facebook',
+        facebook: false,
+        logo: 'images/facebook.png',
+        link: {
+            privacy: 'https://www.facebook.com/about/privacy',
+            terms: ''
+        }
+    },
+    {
+        name: 'Fabric',
+        model: 'fabric',
+        fabric: false,
+        logo: 'images/fabric.png',
+        link: {
+            privacy: 'https://fabric.io/privacy',
+            terms: ''
+        }
+    },
+    {
+        name: 'Crashlytics',
+        model: 'crashlytics',
+        crashlytics: false,
+        logo: 'images/crashlytics.png',
+        link: {
+            privacy: 'http://try.crashlytics.com/terms/privacy-policy.pdf',
+            terms: ''
+        }
+    },
+    {
+        name: 'Matomo',
+        model: 'piwik',
+        piwik: false,
+        logo: 'images/piwik.png',
+        link: {
+            privacy: 'https://matomo.org/privacy-policy/',
+            terms: ''
+        }
+    },
+    {
+        name: 'Clicky',
+        model: 'clicky',
+        clicky: false,
+        logo: 'images/clicky.png',
+        link: {
+            privacy: 'https://clicky.com/terms#privacy',
+            terms: ''
+        }
     }
-  },
-  {
-    name: 'AdMob',
-    model: 'admob',
-    admob: false,
-    logo: 'images/admob.png',
-    link: {
-      privacy: 'https://support.google.com/admob/answer/6128543?hl=en',
-      terms: ''
-    }
-  },
-  {
-    name: 'Firebase Analytics',
-    model: 'firebase',
-    firebase: false,
-    logo: 'images/firebase.png',
-    link: {
-      privacy: 'https://firebase.google.com/policies/analytics',
-      terms: ''
-    }
-  },
-  {
-    name: 'Facebook',
-    model: 'facebook',
-    facebook: false,
-    logo: 'images/facebook.png',
-    link: {
-      privacy: 'https://www.facebook.com/about/privacy',
-      terms: ''
-    }
-  },
-  {
-    name: 'Fabric',
-    model: 'fabric',
-    fabric: false,
-    logo: 'images/fabric.png',
-    link: {
-      privacy: 'https://fabric.io/privacy',
-      terms: ''
-    }
-  },
-  {
-    name: 'Crashlytics',
-    model: 'crashlytics',
-    crashlytics: false,
-    logo: 'images/crashlytics.png',
-    link: {
-      privacy: 'http://try.crashlytics.com/terms/privacy-policy.pdf',
-      terms: ''
-    }
-  },
-  {
-    name: 'Matomo',
-    model: 'piwik',
-    piwik: false,
-    logo: 'images/piwik.png',
-    link: {
-      privacy: 'https://matomo.org/privacy-policy/',
-      terms: ''
-    }
-  },
-  {
-    name: 'Clicky',
-    model: 'clicky',
-    clicky: false,
-    logo: 'images/clicky.png',
-    link: {
-      privacy: 'https://clicky.com/terms#privacy',
-      terms: ''
-    }
-  }
 ]
